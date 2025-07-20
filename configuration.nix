52912b2549d63d2766d2681622b978df137eb927
bR00dLoRRdz <technique321@gmail.com>
2025-07-20 15:59:22 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -98,7 +98,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   google-chrome
-  adobe-reader
+  # adobe-reader
   neofetch
   vim
   
